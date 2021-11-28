6e0f8710915ce2be60773facb5cdfc9b2d83946d
Nadeem Lari <nadeemlari@gmail.com>
2021-11-28 21:01:52 +0530

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RealisticFakeData
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } // full name
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public double Income { get; set; }
+    }
+
+    public class Address
+    {
+        public string Add { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Zip { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bogus;
+
+namespace RealisticFakeData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var addressFaker = new Faker<Address>()
+                    .RuleFor(x=> x.Add,x=> x.Address.FullAddress())
+                    .RuleFor(x=> x.City,x=>x.Address.City())
+                    .RuleFor(x => x.Country, x => x.Address.Country())
+                    .RuleFor(x => x.Zip, x => x.Address.ZipCode())
+                ;
+            addressFaker.Locale =  "en-IN";
+            var data = addressFaker.Generate();
+            
+            Console.Read();
+        }
+    }
+}
